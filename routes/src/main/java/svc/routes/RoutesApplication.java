f242a1df1ddbbcef69a211391ebbe83234288452
Rachitha <47741826+Rachithaa@users.noreply.github.com>
2024-05-29 01:47:21 +0530

    Eureka server -svc

--- routes/src/main/java/svc/routes/RoutesApplication.java
+++ routes/src/main/java/svc/routes/RoutesApplication.java
@@ -2,9 +2,8 @@ package svc.routes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 
 @EnableMongoRepositories
 @SpringBootApplication
